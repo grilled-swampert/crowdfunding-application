cb4b6b8cccc2d989cbcb8dfb9c3691aa07e0ebbe
Shreyas Pachpor <shreyaspachporr@gmail.com>
2024-06-06 13:22:57 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,20 +13,25 @@ import themes from 'themes';
 // project imports
 import NavigationScroll from 'layout/NavigationScroll';
 
+// thirdweb
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { PolygonAmoyTestnet } from '@thirdweb-dev/chains';
 // ==============================|| APP ||============================== //
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themes(customization)}>
-        <CssBaseline />
-        <NavigationScroll>
-          <RouterProvider router={router} />
-        </NavigationScroll>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <ThirdwebProvider activeChain={PolygonAmoyTestnet} clientId="700344b43a6642667fc8f4aafb37096f">
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themes(customization)}>
+          <CssBaseline />
+          <NavigationScroll>
+            <RouterProvider router={router} />
+          </NavigationScroll>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </ThirdwebProvider>
   );
 };
 
